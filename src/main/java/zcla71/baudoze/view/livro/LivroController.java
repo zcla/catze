10c8eb056766b2bb3ed34b6c3a51b1270b3b2e56
zcla <zeclaudioaguiar@gmail.com>
2024-07-01 01:17:19 +0000

    Estava incompleto

--- src/main/java/zcla71/baudoze/view/livro/LivroController.java
+++ src/main/java/zcla71/baudoze/view/livro/LivroController.java
@@ -1,6 +1,8 @@
 package zcla71.baudoze.view.livro;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,34 +17,36 @@ import com.fasterxml.jackson.databind.DatabindException;
 import zcla71.baudoze.controller.BauDoZe;
 import zcla71.baudoze.repository.model.RepositoryException;
 import zcla71.baudoze.view.Pagina.Estado;
+import zcla71.baudoze.view.model.Obras;
 
 @Controller
 public class LivroController {
     private String livroSetModel(Model model, BauDoZe bauDoZe, LivroPagina livro) throws StreamReadException, DatabindException, IOException {
         model.addAttribute("livro", livro);
-        model.addAttribute("obras", bauDoZe.getObras()); // TODO Criar classe equivalente em view, conforme abaixo
-        // new LivroPaginaObra(obra)
-        // Obra obra = service.buscaObraPorId(idObra);
-        // String autores = "";
-        // for (String idAutor : obra.getIdsAutores()) {
-        //     autores += service.buscaPessoaPorId(idAutor).getNome() + ", ";
-        // }
-        // if (autores.length() > 0) {
-        //     autores = " (" + autores.substring(0, autores.length() - 2) + ")";
-        // }
+
+        Collection<Obras> obras = bauDoZe.getObras();
+        Collection<LivroPaginaObra> lpObras = new ArrayList<>();
+        for (Obras obra : obras) {
+            lpObras.add(new LivroPaginaObra(obra));
+        }
+        model.addAttribute("obras", lpObras);
+
         return "livro";
     }
 
     @GetMapping("/livro/{id}")
     public String livroGet(Model model, @PathVariable String id) throws StreamReadException, DatabindException, IOException {
         BauDoZe bauDoZe = BauDoZe.getInstance();
+
         LivroPagina livro = BauDoZe.getInstance().getLivro(id);
+
         return livroSetModel(model, bauDoZe, livro);
     }
 
     @PutMapping("/livro/{id}")
     public String livroPut(Model model, @PathVariable String id, @ModelAttribute LivroForm form) throws StreamReadException, DatabindException, IOException, RepositoryException {
         BauDoZe bauDoZe = BauDoZe.getInstance();
+
         LivroPagina livro = bauDoZe.getLivro(id);
         livro.setTitulo(form.getTitulo());
         if (livro.dadosValidos()) {
@@ -51,6 +55,7 @@ public class LivroController {
         } else {
             livro.setEstadoPagina(Estado.UPDATE);
         }
+
         return livroSetModel(model, bauDoZe, livro);
     }
 }
--- src/main/java/zcla71/baudoze/view/livro/LivroPaginaObra.java
+++ src/main/java/zcla71/baudoze/view/livro/LivroPaginaObra.java
@@ -1,9 +1,18 @@
 package zcla71.baudoze.view.livro;
 
 import lombok.Data;
+import zcla71.baudoze.view.model.Obras;
 
 @Data
 public class LivroPaginaObra {
     private String id;
     private String nome;
+
+    public LivroPaginaObra(Obras obra) {
+        this.id = obra.getId();
+        this.nome = obra.getTitulo();
+        if (obra.getAutorPrincipal() != null) {
+            this.nome += " (" + obra.getAutorPrincipal() + ")";
+        }
+    }
 }
